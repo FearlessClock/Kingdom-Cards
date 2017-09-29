64d4e6f07fa8455bd97f2637bca8bf7b0a775d67
Pieter ALGERA <pieter.algera@devinci.fr>
2017-09-29 13:25:44 +0200

    Made the program location independant of the system

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -4,7 +4,9 @@ import org.apache.logging.log4j.Logger;
 public class MyApp {
 
     public static void main( String[] args ) {
-        System.setProperty("log4j.configurationFile", "C:\\Users\\piete\\IdeaProjects\\tdspLog4J\\src\\main\\resources\\log4j2-test");
+        //Change the "<RollingFile name="rollingfile" fileName="D:\Log4JLogs\smtrace.log"" Line to the location
+        //where you want to save your logs
+        System.setProperty("log4j.configurationFile", "log4j2-test");
         Logger logger = LogManager.getRootLogger();
         logger.trace("Configuration File Defined To Be :: "+System.getProperty("log4j.configurationFile"));
     }
