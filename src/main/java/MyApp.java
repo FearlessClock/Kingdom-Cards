fd989db75f3c12eab74f23e864291ddfcfe3b42b
Pieter ALGERA <pieter.algera@devinci.fr>
2017-09-29 13:51:23 +0200

    Fixed

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -3,11 +3,11 @@ import org.apache.logging.log4j.Logger;
 
 public class MyApp {
 
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         //Change the "<RollingFile name="rollingfile" fileName="D:\Log4JLogs\smtrace.log"" Line to the location
         //where you want to save your logs
         System.setProperty("log4j.configurationFile", "log4j2-test");
         Logger logger = LogManager.getRootLogger();
-        logger.trace("Configuration File Defined To Be :: "+System.getProperty("log4j.configurationFile"));
+        logger.trace("Configuration File Defined To Be :: " + System.getProperty("log4j.configurationFile"));
     }
 }
