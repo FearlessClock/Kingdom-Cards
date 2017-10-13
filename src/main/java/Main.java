506e2f2c7e3bda66231822508bf668ef4a552614
Pieter ALGERA <pieter.algera@devinci.fr>
2017-10-13 15:56:58 +0200

    [+] Added Hello world

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,16 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class MyApp {
+public class Main {
 
     public static void main(String[] args) {
-        //Change the "<RollingFile name="rollingfile" fileName="D:\Log4JLogs\smtrace.log"" Line to the location
+        //Change the "<RollingFile name="rollingfile" fileName="D:\Log4JLogs\smtrace.log"" Line tso the location
         //where you want to save your logs
         System.setProperty("log4j.configurationFile", "log4j2-test");
         Logger logger = LogManager.getRootLogger();
         logger.trace("Configuration File Defined To Be :: " + System.getProperty("log4j.configurationFile"));
+        System.out.print("Hello, world");
+        
     }
 }
 //test commit
