4f0501de35655a1f8d0053fcb97fbfe76afbaff6
Pafy78 <pierre.baudoin@devinci.fr>
2017-10-23 10:57:10 +0200

    [+] Game.fxml [-] mainWindow.fxml [*] Fix path to Game.fxml

--- src/main/java/com/Kingdom/cards/Main.java
+++ src/main/java/com/Kingdom/cards/Main.java
@@ -13,13 +13,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        Parent root = FXMLLoader.load(getClass().getResource("../../../Game.fxml"));
+		Scene scene = new Scene(root,400,400);
+		primaryStage.setTitle("Game");
+		primaryStage.setScene(scene);
+		primaryStage.show();
     }
     public static void main(String[] args) {
-        //Change the "<RollingFile name="rollingfile" fileName="D:\Log4JLogs\smtrace.log"" Line to the location
+        //Change the "<RollingFile name="rollingfile" fileName="D:/Log4JLogs/smtrace.log"" Line to the location
         //where you want to save your logs
         System.setProperty("log4j.configurationFile", "log4j2-test");
         Logger logger = LogManager.getRootLogger();
