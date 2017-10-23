08f837ec32d2599b4ebc1af8476840390bc6b2f6
Pafy78 <pierre.baudoin@devinci.fr>
2017-10-23 11:13:03 +0200

    [+] Add button test draw [*] Change path of Game.fxml

--- src/main/java/com/Kingdom/cards/Controllers/controller.java
+++ src/main/java/com/Kingdom/cards/Controllers/controller.java
@@ -1,4 +1,15 @@
 package com.Kingdom.cards.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class controller {
+	
+	@FXML
+	Button btnDraw;
+	
+	public void DrawCard(ActionEvent event){
+		System.out.println("Card draw");
+	}
 }
--- src/main/java/com/Kingdom/cards/Main.java
+++ src/main/java/com/Kingdom/cards/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../../Game.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Controllers/Game.fxml"));
 		Scene scene = new Scene(root,400,400);
 		primaryStage.setTitle("Game");
 		primaryStage.setScene(scene);
