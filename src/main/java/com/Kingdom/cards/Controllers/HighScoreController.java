40ee11466f38f8e047f86b17abfb48b824adb8e2
Pieter ALGERA <pieter.algera@devinci.fr>
2017-11-01 20:50:10 +0100

    [+] Added controllers for Score and options

---
+++ src/main/java/com/Kingdom/cards/Controllers/HighScoreController.java
@@ -0,0 +1,17 @@
+package com.Kingdom.cards.Controllers;
+
+import javafx.fxml.FXML;
+
+public class HighScoreController {
+
+    public HighScoreController()
+    {
+
+    }
+
+    @FXML
+    public void initialize()
+    {
+
+    }
+}
