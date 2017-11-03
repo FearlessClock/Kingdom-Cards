ba3919ced9eb49781bb04d25835ad291ad212958
Spyk1e <spykie.me@gmail.com>
2017-11-03 14:22:24 +0100

    Removed ToStringTest from TrollTest

--- src/test/java/com/Kingdom/cards/TrollTest.java
+++ src/test/java/com/Kingdom/cards/TrollTest.java
@@ -10,14 +10,5 @@ public class TrollTest {
 	@org.junit.Before
     public void setUp() throws Exception {
     }
-	
-	@Test
-	public void TexttoString() throws Exception{
-		Troll troll = new Troll();
 		
-		Assert.assertEquals(troll.toString(), "You get a Troll\nPower : Swap the cards in front of you with the cards in front of your opponent");
-	}
-	
-	
-	
 }
