1c0760df8a9e326233683e5a6de6f098271e4efd
Spyk1e <spykie.me@gmail.com>
2017-11-03 14:17:24 +0100

    Removed ToString from Dryad

--- src/main/java/com/Kingdom/cards/Model/Dryad.java
+++ src/main/java/com/Kingdom/cards/Model/Dryad.java
@@ -9,11 +9,8 @@ public class Dryad extends Card{
 		this.race = "Dryad";
 	}
 
-	public String toString(){
-		return ("You get a " + race + "\nPower : Steal a card in front of your opponent and add it in front of you without activating its power" );
 
-	}
-	public void power(){
+	public void Power(){
 		// Need to have the class Kingdom
 		// Opponent Kingdom.remove
 		// Our Kingdom.add
