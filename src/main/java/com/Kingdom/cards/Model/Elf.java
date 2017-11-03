0472ff2910ebbb653bc86b1f59e45813df680018
Spyk1e <spykie.me@gmail.com>
2017-11-03 14:17:53 +0100

    Removed ToString from Elf

--- src/main/java/com/Kingdom/cards/Model/Elf.java
+++ src/main/java/com/Kingdom/cards/Model/Elf.java
@@ -8,11 +8,6 @@ public class Elf extends Card{
 		this.race = "Elf";
 	}
 
-	public String toString(){
-		return ("You get a " + race + "\nPower : Copy and use the power of one of the card in front of you" );
-
-	}
-
 	public void Power(){
 		// Need to have the class Kingdom
 		// Select a card in the kingdom
