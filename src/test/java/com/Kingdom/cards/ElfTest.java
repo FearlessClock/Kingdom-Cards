30acbacdd34809b7eb6a7e390df245c14e7b1c9c
Spyk1e <spykie.me@gmail.com>
2017-11-03 14:20:54 +0100

    Removed ToStringTest from ElfTest

--- src/test/java/com/Kingdom/cards/ElfTest.java
+++ src/test/java/com/Kingdom/cards/ElfTest.java
@@ -11,10 +11,4 @@ public class ElfTest {
     public void setUp() throws Exception {
     }
 	
-	@Test
-	public void TexttoString() throws Exception{
-		Elf elf = new Elf();
-		
-		Assert.assertEquals(elf.toString(), "You get a Elf\nPower : Copy and use the power of one of the card in front of you");
-	}
 }
