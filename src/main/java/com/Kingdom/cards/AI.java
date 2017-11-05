56d99cb5630e643ff91c03424f67b4fa60ed165d
Spyk1e <spykie.me@gmail.com>
2017-11-05 14:55:50 +0100

    AI modification
    Added AITest and modified AI class to override PLayCard so it auto
    select a card randomly

--- src/main/java/com/Kingdom/cards/AI.java
+++ src/main/java/com/Kingdom/cards/AI.java
@@ -1,5 +1,6 @@
 package com.Kingdom.cards;
 
+import com.Kingdom.cards.Model.Card;
 import com.Kingdom.cards.Model.Hand;
 import com.Kingdom.cards.Model.Player;
 
@@ -8,5 +9,11 @@ public class AI extends Player {
     public AI() {
         this.pseudo = "";
         hand = new Hand();
+    }
+        
+    public Card PlayCard() {
+    	int nbOfCardsInHand = this.SizeHand();
+    	int cardIndex = (int) (Math.random()*nbOfCardsInHand);
+        return hand.PlayCard(cardIndex);
     }
 }
