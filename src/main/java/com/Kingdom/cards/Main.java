8564d74b60a4c8d8eb6f8b68afed5be8c1d4493d
Pieter Algera <pieter@joyjet.com>
2017-11-07 22:55:57 +0100

    Added cucumber

--- src/main/java/com/Kingdom/cards/Main.java
+++ src/main/java/com/Kingdom/cards/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
     public static void main(String[] args) {
         //Change the "<RollingFile name="rollingfile" fileName="D:/Log4JLogs/smtrace.log"" Line to the location
         //where you want to save your logs
+
         System.setProperty("log4j.configurationFile", "log4j2-test");
         Logger logger = LogManager.getRootLogger();
         logger.trace("Configuration File Defined To Be :: " + System.getProperty("log4j.configurationFile"));
---
+++ src/test/resources/com/Kingdom/cards/Features/Troll.feature
@@ -0,0 +1,6 @@
+Feature: Features of my service
+
+Scenario: Calling my service
+  Given some cards
+  When I play a card
+  Then the card goes away
