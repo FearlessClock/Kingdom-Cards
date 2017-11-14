7a52f3966fc51853845c14b495ba9ede82f04ca6
nbentchi <nourbentchikou@gmail>
2017-11-14 11:32:49 +0100

    [*] Fixed Card graying

--- src/main/java/com/Kingdom/cards/Model/Elf.java
+++ src/main/java/com/Kingdom/cards/Model/Elf.java
@@ -1,12 +1,21 @@
 package com.Kingdom.cards.Model;
 
 import com.Kingdom.cards.Deck;
+import com.Kingdom.cards.Model.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Elf extends Card{
 
 	public Elf() {
 		super();
 		this.race = "Elf";
+		this.view.getStyleClass().add("Elf");
+		this.view.setText(" - Elf - ");
+		System.out.println("Elf added");
+
 	}
 
 	public void power(Board b, Deck d, Player p1, Player p2) {
