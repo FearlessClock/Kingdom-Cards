7d3bb9d6704fc5eac387192862c836834556af04
Spyk1e <spykie.me@gmail.com>
2017-12-19 17:41:30 +0100

    Comments for AI

--- src/main/java/com/Kingdom/cards/AI.java
+++ src/main/java/com/Kingdom/cards/AI.java
@@ -10,21 +10,25 @@ import java.util.Random;
 
 public class AI extends Player {
 
+	//Arguments
 	Random rand;
+	
+	//Constructor
 	public AI() {
 		rand = new Random();
 		this.pseudo = "";
 		hand = new Hand();
 	}
 
+	//Functions
 	public Card PlayCard() {
-		if(this.SizeHand() > 0)
+		if(this.SizeHand() > 0)//Play only if there is cards on his hand
 		{
 			int nbOfCardsInHand = this.SizeHand();
-			int cardIndex = rand.nextInt(nbOfCardsInHand);
-			return hand.playCard(cardIndex);
+			int cardIndex = rand.nextInt(nbOfCardsInHand);//Select a random card to play it
+			return hand.playCard(cardIndex);//Play the selected card
 		}
-		else
+		else//Play no one card
 		{
 			return null;
 		}
